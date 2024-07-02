bfb54f38dd71e14f4be1445406e61be931c0cc4a
Sarah Matishoi <sarah.matishoi@scynett.com>
2024-07-02 14:09:13 +0300

    popup changes

--- js/script.js
+++ js/script.js
@@ -140,3 +140,72 @@ var swiper = new Swiper(".slide-content", {
   },
 });
 
+
+
+
+const form =document.querySelector("form");
+const fullName =document.getElementById("name");
+const email =document.getElementById("email");
+const phone =document.getElementById("phone");
+const subject =document.getElementById("subject");
+const message =document.getElementById("message");
+
+
+function sendEmail(){
+  const bodyMessage = `Full Name: ${fullName.value} <br>
+  Email: ${email.value} <br>
+  Phone Number: ${phone.value} <br>
+  Subject: ${subject.value} <br>
+  Message: ${message.value}`;
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : subject.value,
+    Body : bodyMessage
+}).then(
+  message => {
+    if (message =="OK"){
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully!",
+        icon: "success"
+      });
+    }
+  
+  }
+);
+}
+
+function checkInputs(){
+  const items = document.querySelectorAll(".item");
+  for (const item of items ){
+    if(item.value == ""){
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    item.addEventListener("keyup", () => {
+      if (item.value != ""){
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      }
+      else{
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+
+  }
+
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+
+  // sendEmail();
+  
+});
